cb450bae39fef5c77c0481c36d3fd068b7669cae
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-03-12 10:40:50 +0900

    try ClientTransaction annotation

--- remote-ejb-2/src/main/java/sample/ejb/RemoteB.java
+++ remote-ejb-2/src/main/java/sample/ejb/RemoteB.java
@@ -1,8 +1,11 @@
 package sample.ejb;
 
+import org.jboss.ejb.client.annotation.ClientTransaction;
+import org.jboss.ejb.client.annotation.ClientTransactionPolicy;
 import javax.ejb.Remote;
 
 @Remote
+@ClientTransaction(ClientTransactionPolicy.NOT_SUPPORTED)
 public interface RemoteB {
     void addAndCommit(int id, String bookTitle);
 }
