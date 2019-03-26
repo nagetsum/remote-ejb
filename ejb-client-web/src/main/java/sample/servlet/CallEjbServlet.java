e1c599a19c3c66fdd365f640269139c33be15925
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-03-26 16:40:28 +0900

    polish

--- ejb-client-web/src/main/java/sample/servlet/CallEjbServlet.java
+++ ejb-client-web/src/main/java/sample/servlet/CallEjbServlet.java
@@ -1,7 +1,5 @@
 package sample.servlet;
 
-import sample.ejb.DelegateBean;
-import javax.ejb.EJB;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
