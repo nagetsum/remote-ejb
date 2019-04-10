86d9998dc5307a64b5698203e2c4f647895a3c5a
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-04-10 16:37:46 +0900

    remove chain-call-ejb

--- ejb-client-web/src/main/java/sample/servlet/CallEjbServlet.java
+++ ejb-client-web/src/main/java/sample/servlet/CallEjbServlet.java
@@ -1,24 +1,37 @@
 package sample.servlet;
 
-import sample.ejb.DelegateBean;
+import sample.ejb.RemoteA;
 import javax.ejb.EJB;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Hashtable;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @WebServlet("/test")
 public class CallEjbServlet extends HttpServlet {
 
-    @EJB
-    private DelegateBean delegate;
+    private static final AtomicInteger cnt = new AtomicInteger();
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws IOException {
 
-        int cnt = delegate.callRemoteBean();
+        Hashtable<String, String> jndiProps = new Hashtable<>();
+        jndiProps.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+
+        try {
+            Context context = new InitialContext(jndiProps);
+            RemoteA remoteA = (RemoteA) context.lookup("ejb:/remote-ejb-1/RemoteABean!" + RemoteA.class.getName() + "?stateful");
+            remoteA.addAndCommit(cnt.incrementAndGet(), "test book " + cnt.get());
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
 
         res.setContentType("text/plain; charset=utf-8");
         res.getWriter().write("add book done. No: " + cnt);
